8bf64baf01fb149f5b82597fac78811c84e9d084
NGgmnBQGwR <4226721+NGgmnBQGwR@users.noreply.github.com>
2019-08-31 16:53:21 +0700

    Add basic recursive bruteforce solving method prototype
    It takes too long to finish, need to find ways to improve run time.

--- src/map.rs
+++ src/map.rs
@@ -19,7 +19,7 @@ impl Default for MonolithMap {
 
 impl MonolithMap {
     pub fn solve(self) -> Vec<Tile> {
-        self.solve_threaded_bruteforce()
+        self.solve_recursive_bruteforce()
     }
 
     fn get(&self, x: usize, y: usize) -> u8 {
@@ -192,6 +192,55 @@ impl MonolithMap {
             }
         }
         count
+    }
+
+    pub fn solve_recursive_bruteforce(self) -> Vec<Tile> {
+        type SolvedPath = (u32, Vec<Tile>);
+
+        fn work(
+            results: &mut Vec<SolvedPath>,
+            steps: Vec<Tile>,
+            map: MonolithMap,
+            dead_cells_limit: u32,
+        ) {
+            let groups = map.all_groups();
+            if groups.is_empty() {
+                let count = map.get_dead_tiles_count();
+
+                if results.is_empty() || count < results.first().unwrap().0 {
+                    results.push((count, steps));
+                    results.sort();
+                }
+            } else {
+                for group in groups {
+                    let first_tile = group[0];
+
+                    let mut new_map = map.clone();
+                    new_map.click(first_tile.0, first_tile.1);
+                    if new_map.get_dead_tiles_count() > dead_cells_limit {
+                        continue;
+                    }
+
+                    let new_steps = {
+                        let mut temp = steps.clone();
+                        temp.push(first_tile);
+                        temp
+                    };
+                    work(results, new_steps, new_map, dead_cells_limit);
+                }
+            }
+        }
+
+        let mut results: Vec<SolvedPath> = Vec::with_capacity(100);
+        for max_dead_cells_allowed in [5u32, 10, 15, 20].iter() {
+            let map = self.clone();
+            work(&mut results, Vec::new(), map, *max_dead_cells_allowed);
+            if !results.is_empty() {
+                break;
+            }
+        }
+        results.reverse();
+        results.pop().unwrap_or_default().1
     }
 
     pub fn solve_threaded_bruteforce(self) -> Vec<Tile> {
