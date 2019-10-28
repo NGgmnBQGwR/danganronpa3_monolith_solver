5e714e4ecb368e8cedb7ca80dd582417b44d5557
NGgmnBQGwR <4226721+NGgmnBQGwR@users.noreply.github.com>
2019-10-29 00:45:36 +0700

    Added solver #6
    #5 is a sequential breadth-first search, while this is a random one.

--- src/map/monolith_solver.rs
+++ src/map/monolith_solver.rs
@@ -374,3 +374,113 @@ pub fn solve_5(map: MonolithMap) -> Vec<Tile> {
     results.reverse();
     results.pop().unwrap_or_default().1
 }
+
+/// Recursive Random Multithreaded Bounbed Bruteforce
+pub fn solve_6(map: MonolithMap) -> Vec<Tile> {
+    fn timer_thread(exit_flag: Arc<AtomicBool>, current_best: Arc<AtomicU32>) {
+        let start = Instant::now();
+        loop {
+            if start.elapsed().as_secs() > 60 || current_best.load(Ordering::Relaxed) == 0 {
+                println!("Stopping solver.");
+                exit_flag.store(true, Ordering::Relaxed);
+                break;
+            }
+            thread::sleep(std::time::Duration::from_millis(100));
+        }
+    }
+    fn brute_solver(
+        map: MonolithMap,
+        result: Arc<Mutex<Vec<SolvedPath>>>,
+        current_best: Arc<AtomicU32>,
+        exit_flag: Arc<AtomicBool>,
+    ) {
+        fn work(
+            result: Arc<Mutex<Vec<SolvedPath>>>,
+            steps: Vec<Tile>,
+            map: MonolithMap,
+            current_best: Arc<AtomicU32>,
+            exit_flag: Arc<AtomicBool>,
+            rng: &mut ThreadRng,
+        ) {
+            if exit_flag.load(Ordering::Acquire) {
+                return;
+            }
+
+            let mut groups = map.all_groups();
+            if groups.is_empty() {
+                let count = map.get_dead_tiles_count();
+
+                if count < current_best.load(Ordering::Acquire) {
+                    result.lock().unwrap().push((count, steps));
+                    current_best.store(count, Ordering::Release);
+                    println!("Current best result is: {} tiles remaining.", count);
+                }
+            } else {
+                groups.shuffle(rng);
+                for group in groups {
+                    let first_tile = group[0];
+
+                    let mut new_map = map.clone();
+                    new_map.click(first_tile.0, first_tile.1);
+                    if new_map.get_dead_tiles_count() >= current_best.load(Ordering::Acquire) {
+                        continue;
+                    }
+
+                    let new_steps = {
+                        let mut temp = steps.clone();
+                        temp.push(first_tile);
+                        temp
+                    };
+                    work(
+                        result.clone(),
+                        new_steps,
+                        new_map,
+                        current_best.clone(),
+                        exit_flag.clone(),
+                        rng,
+                    );
+                }
+            }
+        }
+
+        work(
+            result.clone(),
+            Vec::new(),
+            map,
+            current_best.clone(),
+            exit_flag.clone(),
+            &mut thread_rng(),
+        );
+    };
+
+    let result = Arc::new(Mutex::new(Vec::with_capacity(100)));
+    let current_best = Arc::new(AtomicU32::new(22 * 11));
+    let exit_flag = Arc::new(AtomicBool::new(false));
+
+    let timer_handle = {
+        let exit_flag_clone = exit_flag.clone();
+        let best_clone = current_best.clone();
+        thread::spawn(|| timer_thread(exit_flag_clone, best_clone))
+    };
+    let workers: Vec<_> = (0..8)
+        .map(|_| {
+            let map = map.clone();
+            let result_clone = result.clone();
+            let best_clone = current_best.clone();
+            let exit_flag_clone = exit_flag.clone();
+            thread::spawn(|| brute_solver(map, result_clone, best_clone, exit_flag_clone))
+        })
+        .collect();
+
+    timer_handle
+        .join()
+        .expect("Failed to join on a timer thread handle.");
+    for worker in workers {
+        worker.join().expect("Failed to join on a thread handle.");
+    }
+
+    let mut results = result.lock().unwrap();
+    results.sort();
+    results.reverse();
+    results.pop().unwrap_or_default().1
+}
