4ef81c001eab2fa94038920ce80ab95dd15213cd
NGgmnBQGwR <4226721+NGgmnBQGwR@users.noreply.github.com>
2019-10-28 22:17:43 +0700

    Add experimental solver #5
    Still work in progress.
    Uses 100% of CPU and a global "current best", which makes it currently
      the most thought-out approach.
    Currently loses to random walking.

--- src/map/monolith_solver.rs
+++ src/map/monolith_solver.rs
@@ -1,13 +1,13 @@
 use super::monolith_map::{MonolithMap, SolvedPath, Tile};
+use crossbeam::queue::ArrayQueue;
 use rand::rngs::ThreadRng;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
-use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, AtomicU32, Ordering};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 use std::time::Instant;
-
-use crossbeam::queue::ArrayQueue;
 
 /// Recursive Random Singlethreaded Unbounbed Bruteforce
 pub fn solve_1(map: MonolithMap) -> Vec<Tile> {
@@ -238,6 +238,138 @@ pub fn solve_4(map: MonolithMap) -> Vec<Tile> {
     while let Ok(value) = result_queue.pop() {
         results.push(value);
     }
+    results.sort();
+    results.reverse();
+    results.pop().unwrap_or_default().1
+}
+
+/// Recursive Sequential Multithreaded Bruteforce
+pub fn solve_5(map: MonolithMap) -> Vec<Tile> {
+    fn timer_thread(exit_flag: Arc<AtomicBool>, current_best: Arc<AtomicU32>) {
+        let start = Instant::now();
+        loop {
+            if start.elapsed().as_secs() > 60 || current_best.load(Ordering::Relaxed) == 0 {
+                println!("Stopping solver.");
+                exit_flag.store(true, Ordering::Relaxed);
+                break;
+            }
+            thread::sleep(std::time::Duration::from_millis(100));
+        }
+    }
+    fn brute_solver(
+        job_queue: Arc<ArrayQueue<(Vec<Tile>, MonolithMap)>>,
+        result: Arc<Mutex<Vec<SolvedPath>>>,
+        current_best: Arc<AtomicU32>,
+        exit_flag: Arc<AtomicBool>,
+    ) {
+        fn work(
+            result: Arc<Mutex<Vec<SolvedPath>>>,
+            steps: Vec<Tile>,
+            map: MonolithMap,
+            current_best: Arc<AtomicU32>,
+            exit_flag: Arc<AtomicBool>,
+        ) {
+            if exit_flag.load(Ordering::Acquire) {
+                return;
+            }
+
+            let groups = map.all_groups();
+            if groups.is_empty() {
+                let count = map.get_dead_tiles_count();
+
+                if count < current_best.load(Ordering::Acquire) {
+                    result.lock().unwrap().push((count, steps));
+                    current_best.store(count, Ordering::Release);
+                    println!("Current best result is: {} tiles remaining.", count);
+                }
+            } else {
+                for group in groups {
+                    let first_tile = group[0];
+
+                    let mut new_map = map.clone();
+                    new_map.click(first_tile.0, first_tile.1);
+                    if new_map.get_dead_tiles_count() >= current_best.load(Ordering::Acquire) {
+                        continue;
+                    }
+
+                    let new_steps = {
+                        let mut temp = steps.clone();
+                        temp.push(first_tile);
+                        temp
+                    };
+                    work(
+                        result.clone(),
+                        new_steps,
+                        new_map,
+                        current_best.clone(),
+                        exit_flag.clone(),
+                    );
+                }
+            }
+        }
+
+        loop {
+            let (steps, map) = match job_queue.pop() {
+                Ok(job) => job,
+                Err(_) => {
+                    thread::sleep(Duration::from_millis(1_000));
+                    match job_queue.pop() {
+                        Ok(job) => job,
+                        Err(_) => return,
+                    }
+                }
+            };
+            work(
+                result.clone(),
+                steps,
+                map,
+                current_best.clone(),
+                exit_flag.clone(),
+            );
+        }
+    };
+
+    let job_queue = Arc::new(ArrayQueue::new(200));
+    let result = Arc::new(Mutex::new(Vec::with_capacity(100)));
+    let current_best = Arc::new(AtomicU32::new(22 * 11));
+    let exit_flag = Arc::new(AtomicBool::new(false));
+    {
+        let groups = map.all_groups();
+        for group in groups {
+            let first_tile = group[0];
+            let mut new_steps = Vec::with_capacity(1);
+            new_steps.push(first_tile);
+            let mut new_map = map.clone();
+            new_map.click(first_tile.0, first_tile.1);
+            job_queue
+                .push((new_steps, new_map))
+                .expect("Failed to push starting value.");
+        }
+    }
+
+    let timer_handle = {
+        let exit_flag_clone = exit_flag.clone();
+        let best_clone = current_best.clone();
+        thread::spawn(|| timer_thread(exit_flag_clone, best_clone))
+    };
+    let workers: Vec<_> = (0..8)
+        .map(|_| {
+            let job_clone = job_queue.clone();
+            let result_clone = result.clone();
+            let best_clone = current_best.clone();
+            let exit_flag_clone = exit_flag.clone();
+            thread::spawn(|| brute_solver(job_clone, result_clone, best_clone, exit_flag_clone))
+        })
+        .collect();
+
+    timer_handle
+        .join()
+        .expect("Failed to join on a timer thread handle.");
+    for worker in workers {
+        worker.join().expect("Failed to join on a thread handle.");
+    }
+
+    let mut results = result.lock().unwrap();
     results.sort();
     results.reverse();
     results.pop().unwrap_or_default().1
